691340949918bd051469b70a351bacbc8389ee11
allen_shao <Allen_Shao@trendmicro.com>
2022-04-29 16:52:16 +0800

    chore: Init project - config & Api

--- mock/api/index.js
+++ mock/api/index.js
@@ -1,11 +1,3 @@
 module.exports = {
-  dashboard: require("./dashboard"),
-  policy: require("./policy"),
-  provision: require("./provision"),
-  global: require("./global"),
-  log: require("./log"),
-  notification: require("./notification"),
-  auth: require("./auth"),
   account: require("./account"),
-  org: require("./org"),
 };
--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from "./logo.svg";
 import "./App.less";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <header className="App-header">hello world</header>
     </div>
   );
 }
